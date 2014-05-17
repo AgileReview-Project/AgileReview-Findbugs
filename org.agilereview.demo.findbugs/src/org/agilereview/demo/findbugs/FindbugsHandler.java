4fcbc88a362bc5da9063bf638c3567634d132c14
Peter Reuter <piet@difficultdecisions.de>
2014-05-17 18:10:58 +0200

    Find bugs with corresponding source lines and file. Without AR integration.

--- org.agilereview.demo.findbugs/src/org/agilereview/demo/findbugs/FindbugsHandler.java
+++ org.agilereview.demo.findbugs/src/org/agilereview/demo/findbugs/FindbugsHandler.java
@@ -1,14 +1,20 @@
 package org.agilereview.demo.findbugs;
 
+import org.agilereview.core.external.storage.CommentingAPI;
+import org.agilereview.core.external.storage.Review;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.ILog;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.viewers.ISelection;
@@ -16,17 +22,22 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import de.tobject.findbugs.FindbugsPlugin;
+import edu.umd.cs.findbugs.BugAnnotation;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.SortedBugCollection;
+import edu.umd.cs.findbugs.SourceLineAnnotation;
 
 public class FindbugsHandler extends AbstractHandler implements IHandler {
+    
+    /** The Logger for this plugin */
+    private final ILog log = Activator.getDefault().getLog();
     
     @Override
     public Object execute(final ExecutionEvent event) throws ExecutionException {
         
         final ISelection selection = HandlerUtil.getCurrentSelection(event);
         
-        Job findbugsJob = new Job("AgileReview Findbugs Job") {
+        Job findbugsJob = new Job("AgileReview Findbugs") {
             
             @Override
             protected IStatus run(IProgressMonitor monitor) {
@@ -42,12 +53,26 @@ public class FindbugsHandler extends AbstractHandler implements IHandler {
                 if (project != null) {
                     try {
                         SortedBugCollection bugs = FindbugsPlugin.getBugCollection(project, monitor);
+                        Review r = CommentingAPI.createReview();
+                        r.setDescription("FindBugs found " + bugs.getCollection().size() + " bugs in project '" + bugs.getProject().getProjectName()
+                                + "'.");
                         for (BugInstance bug : bugs) {
-                            System.out.println(bug);
+                            System.out.println("\t" + bug.getMessageWithoutPrefix());
+                            for (BugAnnotation annotation : bug.getAnnotationsForMessage(true)) {
+                                if (annotation instanceof SourceLineAnnotation) {
+                                    String fileName = ((SourceLineAnnotation) annotation).getSourceFile();
+                                    Path path = new Path(fileName);
+                                    IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+                                    
+                                    // XXX this does not work atm
+                                    // Comment c = CommentingAPI.createComment("findbugs", r.getId());
+                                    // c.setCommentedFile(file);
+                                }
+                            }
                         }
                     } catch (CoreException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
+                        FindbugsHandler.this.log.log(new Status(IStatus.ERROR, Activator.PLUGIN_ID,
+                                "CoreException occured while collecting bug information into AgileReview.\n" + e.getStackTrace()));
                     }
                 }
                 return new Status(IStatus.OK, Activator.PLUGIN_ID, "Done");
